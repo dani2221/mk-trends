f645d84850b425baea21086e9eb6e8a22d6761e4
Danilo Najkov <danilo.najkov@gmail.com>
2022-12-01 14:23:28 +0100

    init webapp

---
+++ backend/webapp/src/test/java/mk/trends/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.trends.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
